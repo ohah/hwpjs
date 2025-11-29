65925c9ee00f4d253360a0b923584697b897043a
ohah <bookyoon173@gmail.com>
2025-11-30 01:45:54 +0900

    refactor: remove old ReactNative module files
    - Remove ReactNative.podspec
    - Remove ReactNativePackage.kt
    - Remove ReactNative_stub files
    - Remove CxxReactNativeModule files
    - Remove react_native_impl.rs
    - Remove old ReactNativeModuleProvider.mm
    - Remove old src/NativeReactNative.ts and src/index.ts

--- packages/hwpjs/android/stubs/ReactNative_stub.cpp
+++
@@ -1,9 +0,0 @@
-#include "ReactNative_stub.h"
-
-namespace facebook::react {
-
-std::shared_ptr<TurboModule> ReactNative_stub_ModuleProvider(const std::string &moduleName, const JavaTurboModule::InitParams &params) {
-  return nullptr;
-}
-
-} // namespace facebook::react
