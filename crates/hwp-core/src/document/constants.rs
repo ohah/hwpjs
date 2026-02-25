03fd31aac21e655cb06f1eb0cebe673d1d7c1788
ohah <bookyoon173@gmail.com>
2026-02-25 18:16:35 +0900

    test(core): add constants module unit test (HWPTAG_BEGIN)

--- crates/hwp-core/src/document/constants.rs
+++ crates/hwp-core/src/document/constants.rs
@@ -3,3 +3,13 @@
 /// HWPTAG_BEGIN = 0x10 (한글 내부 사용을 위해 예약된 영역의 시작) / HWPTAG_BEGIN = 0x10 (start of reserved area for HWP internal use)
 /// 데이터 레코드 시작 값 / Data record start value
 pub const HWPTAG_BEGIN: u16 = 0x10;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hwptag_begin_value() {
+        assert_eq!(HWPTAG_BEGIN, 0x10);
+    }
+}
